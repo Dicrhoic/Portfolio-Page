95fa4aae93f8ede3e7620099dacaec784da5f218
Kevin <88888289+Dicrhoic@users.noreply.github.com>
2022-08-24 21:14:38 +1000

    Added login checker v1

--- scripts/gachaLogFunc.js
+++ scripts/gachaLogFunc.js
@@ -1,5 +1,5 @@
-function addDateToForm()
-{   
+/*
+function addDateToForm() {
     console.log("loading");
     const d = new Date();
     console.log(d);
@@ -11,103 +11,194 @@ function addDateToForm()
     document.getElementsByName('gachaDate').value = date;
 }
 
-function loadCharacterDropdown()
-{   
-    var date = document.getElementById('gachatime').value
-    console.log(date);
-    if(document.getElementById('vol').value !=0)
-    {
-        console.log("Data is avaliable");
-        addCharsToDropDown();
+*/
+
+function loadCharacterDropdown() {
+    if(loggedUser == "")
+    {   
+        checkLoginStatus();
     }
+    else
+    {
+        var date = document.getElementById('gachatime').value
+        console.log(date);
+        if (document.getElementById('vol').value != 0) {
+            console.log("Data is avaliable");
+            addCharsToDropDown();
+        }
+    }
+
 }
 
-function addCharsToDropDown()
-{
+function addCharsToDropDown() {
     var xmlFile = 'SSRCharacters.xml';
-	var xmlDoc, parser, xmlU, xmlP;
-	parser = new DOMParser();
-	var xhttp = new XMLHttpRequest();
-	xhttp.open("GET", xmlFile, true);
-	xhttp.send();
-	xhttp.onreadystatechange = function () {
-		if (this.readyState === 4 && this.status === 200) {
-			console.log(xhttp.responseText);
-			fillDropdown(this);
-		}
-	}
+    var xmlDoc, parser, xmlU, xmlP;
+    parser = new DOMParser();
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", xmlFile, true);
+    xhttp.send();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            //console.log(xhttp.responseText);
+            fillDropdown(this);
+        }
+    }
 }
 
 const characterList = new Map([
     ["apples", 500],
-  ]);
+]);
 
-function fillDropdown(xml)
-{   
+function fillDropdown(xml) {
 
     characterList.clear();
     var txt = " ";
-    var xmlDoc = xml.responseXML;   
-	var a = xmlDoc.documentElement;
-	var y = xmlDoc.documentElement.childNodes;
+    var xmlDoc = xml.responseXML;
+    var a = xmlDoc.documentElement;
+    var y = xmlDoc.documentElement.childNodes;
     x = xmlDoc.documentElement.childNodes;
     var myDiv = document.getElementById('characterAdderSeg');
     document.getElementById('characterAdderSeg').style.display = "inline";
     //var selectList = document.createElement("select");
     //selectList.setAttribute("id", "characterList");
     //myDiv.appendChild(selectList);
+    var index = 0;
     var current = 0;
-    for (i = 0; i < x.length ;i++) {
+    for (i = 0; i < x.length; i++) {
         for (z = 0; z < y[i].childNodes.length; z++) {
-			if (y[i].childNodes[z].nodeType != 3) {
-				txt += "Node " + i + " Nodename: " + y[i].childNodes[z].nodeName +
-					" (value at index " + z + ": " + y[i].childNodes[z].childNodes[0].nodeValue + ")";
-				console.log(txt);
-				txt = "";
-                if(z == 1)
-                {
-                    var name = y[i].childNodes[1].childNodes[0].nodeValue; 
+            if (y[i].childNodes[z].nodeType != 3) {
+                txt += "Node " + i + " Nodename: " + y[i].childNodes[z].nodeName +
+                    " (value at index " + z + ": " + y[i].childNodes[z].childNodes[0].nodeValue + ")";
+                //console.log(txt);
+                txt = "";
+                if (z == 1) {
+                    var name = y[i].childNodes[1].childNodes[0].nodeValue;
                     var image = y[i].childNodes[5].childNodes[0].nodeValue;
                     let newChrter = new Character(name, image);
-                    characterList.set(i,newChrter);
-                    console.log(characterList.get(i));
-                    var selectList = document.getElementById('characterList');
-                    var option = document.createElement("option");
-                    option.setAttribute("value", name);
-                    option.text = name;
-                    selectList.appendChild(option);
-                    console.log("Appended " + i + " times");
-                    console.log("Image link: " + newChrter.characterImage());
+                    characterList.set(index, newChrter);
+                    //console.log("This class is: " + characterList.get(i));
+                    console.log(txt);
+                    txt = "";
+                    if (z == 1) {
+                        var name = y[i].childNodes[1].childNodes[0].nodeValue;
+                        var image = y[i].childNodes[5].childNodes[0].nodeValue;
+                        let newChrter = new Character(name, image);
+                        characterList.set(i, newChrter);
+                        console.log(characterList.get(i));
+                        var selectList = document.getElementById('characterList');
+                        var option = document.createElement("option");
+                        option.setAttribute("value", name);
+                        option.text = name;
+                        selectList.appendChild(option);
+                        //console.log("Appended " + i + " times");
+                        console.log("Image link: " + newChrter.characterImage());
+                        console.log("Character Index: " + index);
+                        index++;
+                        console.log("Appended " + i + " times");
+                        console.log("Image link: " + newChrter.characterImage());
+                    }
+
                 }
- 
-			}
-		}
+            }
+        }
+
+    }
+
+    console.log(characterList.size);
+}
+
+/**
+ * Here an import process is used for our mock session
+ */
+
+
+
+function loadGachaLogFile()
+{   
+    if(loggedUser == "")
+    {
+        loginAuth();
+        return; 
+    }
+    var xmlFile = 'GachaLog.xml';
+    parser = new DOMParser();
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("GET", xmlFile, true);
+    xhttp.send();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200) {
+            //console.log(xhttp.responseText);
+        }
     }
 }
 
-function loadCharacterImage()
+function addGachaOutput(xml)
 {
+    var xmlDoc = xml.responseXML;
+    var a = xmlDoc.documentElement;
+}
+
+let characterNumber = 0;
+
+function addCharacterToWall() {
     let index = document.getElementById("characterList").selectedIndex;
     console.log("Selected char is at " + index);
     var character = characterList.get(index);
-    console.log(character.characterImage())
-    document.getElementById('characterIcon').src = character.characterImage();
-  
+    var myDiv = document.getElementById('characterWall');
+    var characterArt = document.createElement('img');
+    characterArt.setAttribute('src', character.characterImage());
+    var idName = "char " + characterNumber.toString();
+    //console.log(idName);
+    characterArt.setAttribute("onclick", "deleteCharArt(this);");
+    characterArt.setAttribute('width', "150");
+    characterArt.setAttribute('width', "86");
+    characterArt.setAttribute("id", idName);
+    myDiv.append(characterArt);
+    characterNumber++;
 }
 
-class Character
+function deleteCharArt(ele)
 {
-    constructor(name, link)
-    {
+    var id = ele.id;
+    var element = document.getElementById(id);
+    console.log("removing " + id);
+    element.remove();
+    console.log("Gone");
+
+}
+
+function loadCharacterImage() {
+    let index = document.getElementById("characterList").selectedIndex;
+    console.log("Selected char is at " + index);
+    var character = characterList.get(index);
+    console.log(character.characterImage());
+    document.getElementById('characterIcon').src = character.characterImage();
+
+}
+
+class Character {
+    constructor(name, link) {
         this.name = name;
-        this.link = link;   
+        this.link = link;
     }
-    characterImage()
-    {
+    characterImage() {
         return this.link;
     }
-    characterName()
-    {
+    characterName() {
         return this.name;
+    }
+}
+
+class Characters {
+    constructor() {
+        this.characters = [];
+    }
+    newCharacter(name, link) {
+        let c = new Character(name, link);
+        this.characters.push(c);
+        return c;
+    }
+    getCharacter(index) {
+        return this.characters[index];
     }
 }
