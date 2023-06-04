87bf84227afbcf1b97871cd6c18702a287d8bf2d
Kevin <88888289+Dicrhoic@users.noreply.github.com>
2023-06-04 20:58:07 +1000

    Added modal

---
+++ functions/HomePage/home.js
@@ -0,0 +1,9 @@
+function LoadModal() {
+    var modal = document.getElementById("infoModal");
+    modal.style.display = "block";
+}
+
+function CloseModal() {
+    var modal = document.getElementById("infoModal");
+    modal.style.display = "none";
+}
