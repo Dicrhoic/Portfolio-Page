3140cdd3aee7d20397aa22c199ca054232e6d030
Kevin <88888289+Dicrhoic@users.noreply.github.com>
2023-06-04 22:00:11 +1000

    Working on filter

--- functions/HomePage/home.js
+++ functions/HomePage/home.js
@@ -1,3 +1,4 @@
+
 function LoadModal() {
     var modal = document.getElementById("infoModal");
     modal.style.display = "block";
@@ -7,3 +8,33 @@ function CloseModal() {
     var modal = document.getElementById("infoModal");
     modal.style.display = "none";
 }
+
+function LoadFilter() {
+    console.log("Filtering");
+    var f = document.getElementById("portfolio");  
+
+}
+
+
+
+
+function SearchName() {
+
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }
+    }
+  }
